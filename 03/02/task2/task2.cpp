a70d103b2fec5fd90d10bdc0da0b2c9e874c1b13
Kosmodesant <smirnov.nal@outlook.com>
2023-01-29 12:21:02 +0300

    initializing a variable

--- 03/02/task2/task2.cpp
+++ 03/02/task2/task2.cpp
@@ -11,14 +11,14 @@ public:
 	void minus_counter() { counter_--; }
 	void print_counter() {cout << counter_ << endl;}
 private:
-	int counter_;
+	int counter_=0;
 };
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	system("chcp 1251");
-	int enter_sign;
+	int enter_sign = 0;
 
 	string enter_console;
 	string yes = "да";
@@ -37,12 +37,17 @@ int main()
 		cin >> enter_sign;
 		counter.set_counter(enter_sign);
 	}
+	else 
+	{
+		cout << "До свидания!";
+		return 0;
+	}
 
 	while (true)
 	{
 		cout << "Введите команду ('+', '-', '=' или 'x'): ";
 		cin >> enter_console;
-		if (enter_console == stop) {cout << "До свидания!"; break; }
+		if (enter_console == stop) { cout << "До свидания!"; break; }
 		else if (enter_console == plus) { counter.plus_counter(); }
 		else if (enter_console == minus) { counter.minus_counter(); }
 		else if (enter_console == equal) { counter.print_counter(); }
