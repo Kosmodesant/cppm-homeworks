fa1efe2bedccd1cfdb897f1cfc1067b67ae0cb29
Kosmodesant <smirnov.nal@outlook.com>
2023-01-29 11:38:49 +0300

    some bug fix at class Calculator

--- 03/01/task1/task1.cpp
+++ 03/01/task1/task1.cpp
@@ -5,42 +5,38 @@ using namespace std;
 
 class Calculator
 {
-public:
+private:
 	double num1;
 	double num2;
-
+public:
+	double get_num1() { return num1; };
+	double get_num2() { return num2; };
 	//метод должен возвращать результат сложения num1 и num2
 	double add() {
-		cout << "num1 + num2 = " << num1 + num2 << endl;
-		return 0;
+		return num1 + num2;
 	};
 
 	//метод должен возвращать результат перемножения num1 и num2
 	double multiply() {
-		cout << "num1 * num2 = " << num1 * num2 << endl;
-		return 0;
+		return num1 * num2;
 	};
 
 	//метод должен возвращать результат вычитания num2 из num1
 	double subtract_1_2() {
-		cout << "num1 - num2 = " << num1 - num2 << endl;
-		return 0;
+		return num2 - num1;
 	};
 
 	//метод должен возвращать результат вычитания num1 из num2
 	double subtract_2_1() {
-		cout << "num2 - num1 = " << num2 - num1 << endl;
-		return 0;
+		return num1 - num2;
 	};
 	//метод должен возвращать результат деления num1 на num2
 	double divide_1_2() {
-		cout << "num1 / num2 = " << num1 / num2 << endl;
-		return 0;
+		return num1 / num2;
 	};
 	//метод должен возвращать результат деления num2 на num1
 	double divide_2_1() {
-		cout << "num2 / num1 = " << num2 / num1 << endl;
-		return 0;
+		return num2 / num1;
 	};
 	//метод должен установить значение поля num1 равное значению аргумента num1 в случае, 
 	//если значение аргумента не равно 0. Возвращает true, если установка произошла успешно, в обратном случае false
@@ -108,12 +104,14 @@ int main()
 				break;
 			}
 		}
-		calculator.add();
-		calculator.multiply();
-		calculator.subtract_1_2();
-		calculator.subtract_2_1();
-		calculator.divide_1_2();
-		calculator.divide_2_1();
+
+		cout << "num1 + num2 = " << calculator.add() << endl
+			<< "num1 - num2 = " << calculator.multiply() << endl
+			<< "num2 - num1 = " << calculator.subtract_1_2() << endl
+			<< "num1 * num2 = " << calculator.subtract_2_1() << endl
+			<< "num1 / num2 = " << calculator.divide_1_2() << endl
+			<< "num2 / num1 = " << calculator.divide_2_1() << endl;
+
 	}
 }
 
