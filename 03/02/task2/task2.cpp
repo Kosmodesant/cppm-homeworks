137da7de3b9172630b69dddb11525fdb2c6603c4
Kosmodesant <smirnov.nal@outlook.com>
2023-02-02 09:35:44 +0300

    switch-case method added

--- 03/02/task2/task2.cpp
+++ 03/02/task2/task2.cpp
@@ -9,48 +9,79 @@ public:
 	void set_counter(int x) { counter_ = x; }
 	void plus_counter() { counter_++; }
 	void minus_counter() { counter_--; }
-	void print_counter() {cout << counter_ << endl;}
+	void print_counter() { cout << counter_ << endl; }
 private:
-	int counter_=0;
+	int counter_ = 0;
 };
 
 int main()
 {
 	setlocale(LC_ALL, "Russian");
 	system("chcp 1251");
-	int enter_sign = 0;
+	int enter_sign = 1;
 
 	string enter_console;
+	char enter_console2;
 	string yes = "да";
 	string no = "нет";
-	string stop = "х";
-	string plus = "+";
-	string minus = "-";
-	string equal = "=";
+	char stop = 'х';
+	char plus = '+';
+	char minus = '-';
+	char equal = '=';
 
 	Сounter counter;
 
-	cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: "; 
+	cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
 	cin >> enter_console;
+
 	if (enter_console == yes) {
 		cout << "Введите начальное значение счётчика: ";
 		cin >> enter_sign;
 		counter.set_counter(enter_sign);
 	}
-	else 
+	else if (enter_console == no) {
+		counter.set_counter(enter_sign);
+	}
+	else
 	{
-		cout << "До свидания!";
+		cout << "Неизвестная команда!";
 		return 0;
 	}
+
 
 	while (true)
 	{
 		cout << "Введите команду ('+', '-', '=' или 'x'): ";
-		cin >> enter_console;
-		if (enter_console == stop) { cout << "До свидания!"; break; }
-		else if (enter_console == plus) { counter.plus_counter(); }
-		else if (enter_console == minus) { counter.minus_counter(); }
-		else if (enter_console == equal) { counter.print_counter(); }
-		else {cout << "Неизвестная команда!" << endl; continue; }
+		cin >> enter_console2;
+
+
+		//if (enter_console == stop) { cout << "До свидания!"; break; }
+		//else if (enter_console == plus) { counter.plus_counter(); }
+		//else if (enter_console == minus) { counter.minus_counter(); }
+		//else if (enter_console == equal) { counter.print_counter(); }
+		//else {cout << "Неизвестная команда!" << endl; continue; }
+
+
+
+		switch (enter_console2)
+		{
+		case '+': // +1
+			counter.plus_counter();
+			break;
+		case '-': // -1
+			counter.minus_counter();
+			break;
+		case '=': // =
+			counter.print_counter();
+			system("pause");
+			break;
+		case 'х': // stop
+			cout << "До свидания!\n" << endl;
+			break;
+		default:  // неизвестная команда
+			cout << "\nНеизвестная команда!\n" << endl;
+			system("pause");
+			continue;
+		}
 	}
 }
