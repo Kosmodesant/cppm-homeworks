8577e391222ddd0ee3548050e3e159ed31fbfd83
Kosmodesant <smirnov.nal@outlook.com>
2023-02-03 15:14:42 +0300

    release 1.0

---
+++ 05/01/task1/task1.cpp
@@ -0,0 +1,40 @@
+﻿#include <iostream>
+#include <string>
+using namespace std;
+
+class Figure {
+public:
+	Figure() { sides_count = 0; this->name = "Фигура: "; };
+	int get_sides_count() { return sides_count; };
+	string get_name() { return name; };
+protected:
+	int sides_count;
+	string name;
+};
+
+
+class Triangle : public Figure {
+public:
+	Triangle() { sides_count = 3; this->name = "Треугольник: "; };
+};
+
+
+class Quadrangle : public Figure {
+public:
+	Quadrangle() { sides_count = 4; this->name = "Квадрат: "; };
+};
+
+
+int main() {
+
+	setlocale(LC_ALL, "Russian");
+
+	Figure unknown = {};
+	Triangle triangle = {};
+	Quadrangle quadrangle = {};
+
+	cout << "Количество сторон:\n";
+	cout << unknown.get_name() << unknown.get_sides_count() << "\n";
+	cout << triangle.get_name() << triangle.get_sides_count() << "\n";
+	cout << quadrangle.get_name() << quadrangle.get_sides_count() << "\n";
+}
