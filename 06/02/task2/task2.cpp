04c399aa71f3f50b6d2cebc62adcbbaf34be93d9
Kosmodesant <smirnov.nal@outlook.com>
2023-02-06 16:52:42 +0300

    some bug fix 3

--- 06/02/task2/task2.cpp
+++ 06/02/task2/task2.cpp
@@ -11,58 +11,35 @@ int main()
 	int enter_sign = 1;
 
 	string enter_console;
-	char enter_console2;
 	string yes = "да";
 	string no = "нет";
-	char stop = 'х';
-	char plus = '+';
-	char minus = '-';
-	char equal = '=';
-
+	string stop = "х";
+	string plus = "+";
+	string minus = "-";
+	string equal = "=";
 	Counter counter;
-
 	cout << "Вы хотите указать начальное значение счётчика? Введите да или нет: ";
 	cin >> enter_console;
-
 	if (enter_console == yes) {
 		cout << "Введите начальное значение счётчика: ";
 		cin >> enter_sign;
 		counter.set_counter(enter_sign);
 	}
-	else if (enter_console == no) {
-		counter.set_counter(enter_sign);
-	}
 	else
 	{
-		cout << "Неизвестная команда!";
-		return 0;
+		counter.set_counter(enter_sign);
+		/*cout << "До свидания!";
+		return 0;*/
 	}
 
 	while (true)
 	{
 		cout << "Введите команду ('+', '-', '=' или 'x'): ";
-		cin >> enter_console2;
-
-		switch (enter_console2)
-		{
-		case '+': // +1
-			counter.plus_counter();
-			break;
-		case '-': // -1
-			counter.minus_counter();
-			break;
-		case '=': // =
-			counter.print_counter();
-			system("pause");
-			break;
-		case 'х': // stop
-			cout << "До свидания!\n" << endl;
-			break;
-		default:  // неизвестная команда
-			cout << "\nНеизвестная команда!\n" << endl;
-			system("pause");
-			continue;
-		}
+		cin >> enter_console;
+		if (enter_console == stop) { cout << "До свидания!"; break; }
+		else if (enter_console == plus) { counter.plus_counter(); }
+		else if (enter_console == minus) { counter.minus_counter(); }
+		else if (enter_console == equal) { counter.print_counter(); }
+		else { cout << "Неизвестная команда!" << endl; continue; }
 	}
 }
-
