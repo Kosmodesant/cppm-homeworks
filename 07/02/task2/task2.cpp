b51e2a33761749747f707062dd95e34d1f7f6620
Kosmodesant <smirnov.nal@outlook.com>
2023-02-10 23:35:48 +0300

    Release 1.0

---
+++ 07/02/task2/task2.cpp
@@ -0,0 +1,15 @@
+﻿#include <iostream>
+
+using namespace std;
+
+#define SUB(a,b) ((a)-(b))
+
+int main()
+{
+	int a = 6;
+	int b = 5;
+	int c = 2;
+	cout << SUB(a, b) << endl;
+	cout << SUB(a, b) * c << endl;
+	cout << SUB(a, b + c) * c << endl;
+}
