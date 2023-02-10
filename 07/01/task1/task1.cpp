14b8654f5d6ca3139eecdf8d933836cf5176c6d4
Kosmodesant <smirnov.nal@outlook.com>
2023-02-10 23:18:33 +0300

    Release 1.0

---
+++ 07/01/task1/task1.cpp
@@ -0,0 +1,39 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+#define MODE 1
+#ifndef MODE
+#error MODE небыл оределен
+#endif
+
+#if MODE == 1
+
+int add(int a, int b)
+{
+	return a + b;
+}
+#endif
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+
+#if MODE==0
+	cout << "Работаю в режиме тренировки";
+	return 0;
+#elif MODE==1
+	int a = 0, b = 0;
+	cout << "Работаю в боевом режиме" << "\n";
+	cout << "Введите число 1: " << "\n";
+	cin >> a;
+	cout << "Введите число 2: " << "\n";
+	cin >> b;
+	cout << "Результат сложения: " << add(a, b) << "\n";
+	return 0;
+#else
+	cout << "Неизвестный режим. Завершение работы";
+	return 1;
+#endif
+}
