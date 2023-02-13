9b010e176d22ad6a7dc2571d3293ba6e8e7eb452
Kosmodesant <smirnov.nal@outlook.com>
2023-02-13 14:45:46 +0300

    Release 1.0

---
+++ 08/01/task1/task1.cpp
@@ -0,0 +1,32 @@
+﻿#include <iostream>
+#include <string>
+
+using namespace std;
+
+int function(string str, int forbidden_length) {
+	if (forbidden_length == str.length()) { throw exception("Вы ввели слово запретной длины! До свидания"); }
+	return str.length();
+}
+
+int main()
+{
+	setlocale(LC_ALL, "Russian");
+	string str;
+	int forbidden_length;
+
+	cout << "Введите запретную длину : \n";
+	cin >> forbidden_length;
+
+	try
+	{
+		std::cout << "Введите слово: ";
+		std::cin >> str;
+
+		std::cout << "Длина слова " << '"' << str << '"' << " равна " << function(str, forbidden_length);
+	}
+	catch (const exception& err)
+	{
+		cout << err.what() << endl;
+	}
+}
+
