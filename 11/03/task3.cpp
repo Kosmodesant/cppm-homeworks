07b6a86fcbe7854e6d4acd4009d2e847955d5531
Kosmodesant <smirnov.nal@outlook.com>
2023-02-27 19:00:23 +0300

    Initial commit

---
+++ 11/03/task3.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+#include <stdlib.h>
+#include <windows.h>
+#include <iostream>
+#include <windows.h>
+#include <locale.h>
+#include "LeaverDynamicLibrary.h"
+
+using namespace std;
+
+int main()
+{
+	SetConsoleCP(1251);
+	SetConsoleOutputCP(1251);
+
+	string name;
+	Leaver leaver;
+
+	cout << "¬ведите им€: " << name << endl;
+	cin >> name;
+	cout << leaver.leave(name);
+
+	return 0;
+}
